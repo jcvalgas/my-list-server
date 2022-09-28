4b6f2b4e1ff5a8a5beef1593de06b3f3b3077020
J.C.Valgas <joaovitorcarvalhovalgas@gmail.com>
2022-09-28 14:50:33 -0300

    feat: deployt changes

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,6 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.port || 3333);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
